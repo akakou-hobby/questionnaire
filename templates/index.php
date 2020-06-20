eab3297881bbc60f1a01bd2c48b3968ddb3b0388
akakou <akakou571@gmail.com>
2020-06-20 14:52:10 +0900

    [add] Firebase configuration

--- templates/index.php
+++ templates/index.php
@@ -5,7 +5,15 @@
     <title>questionnaire</title>
 
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-    <script src="./static/js/auth.js"></script>
+
+    <script src="https://www.gstatic.com/firebasejs/6.2.0/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/6.2.0/firebase-auth.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/6.2.0/firebase-firestore.js"></script>
+
+    <script src="./static/js/firebase.js"></script>
+
+    <script src="./static/js/components/auth.js"></script>
+
   </head>
   <body>
     <div id="auth">
