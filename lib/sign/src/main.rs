284f42f0ef9e6cc0ebe7825cbfea2c9a9e8f69a3
akakou <akakou571@gmail.com>
2020-06-23 00:45:26 +0900

    [add] Verify signature

--- lib/sign/src/main.rs
+++ lib/sign/src/main.rs
@@ -21,7 +21,6 @@ fn sign() {
 
     let mut encoded_digest = String::new();
     io::stdin().read_line(&mut encoded_digest).unwrap();
-
     encoded_digest.truncate(encoded_digest.len() - 1);
 
     let signer_priv_key = fs::read_to_string("/root/pri.pem").unwrap();
@@ -36,11 +35,39 @@ fn sign() {
 }
 
 
+fn verify() {
+    let mut message = String::new();
+    io::stdin().read_line(&mut message).unwrap();
+    message.truncate(message.len() - 1);
+
+    let mut signature = String::new();
+    io::stdin().read_line(&mut signature).unwrap();
+    signature.truncate(signature.len() - 1);
+    let signature = base64::decode(signature).unwrap();
+    
+    let mut signer_pub_key = String::new();
+    io::stdin().read_line(&mut signer_pub_key).unwrap();
+    signer_pub_key.truncate(signer_pub_key.len() - 1);
+    let signer_pub_key = base64::decode(signer_pub_key).unwrap();
+    let signer_pub_key = pem::parse(signer_pub_key).unwrap();
+    let signer_pub_key = RSAPublicKey::from_pkcs1(&signer_pub_key.contents).unwrap();
+
+    let digest = blind::hash_message::<Sha256, _>(&signer_pub_key, message.as_bytes()).unwrap();
+
+    match blind::verify(&signer_pub_key, &digest, &signature) {
+        Ok(_) => { print!("verified") },
+        Err(e) => { print!("{}", e) },
+    };
+}
+
+
+
 pub fn main() {
     for (index, arg) in env::args().enumerate() {
         if index == 1 {
             match arg.as_ref() {
                 "sign" => { sign() },
+                "verify" => { verify() },
                 _ => {}
             }
         }
--- public/static/js/signature.js
+++ public/static/js/signature.js
@@ -7,17 +7,12 @@ setTimeout(async () => {
   const dataStr = blind("hello", res.data);
   const data = JSON.parse(dataStr);
 
-  // localStorage.unblinder = data.unblinder;
-
-  console.log(data.blinded_digest, data.unblinder);
-
   const params = new URLSearchParams();
   params.append("blind_digest", data.blinded_digest);
 
   const _res = await axios.post("/sign", params);
-  console.log(_res.data);
 
   _res.data = _res.data.slice(0, -1);
   const result = unblind(_res.data, res.data, data.unblinder);
-  console.log(result);
+  console.log("signature:", result);
 }, 3000);
