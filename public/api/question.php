ad02894ad406d50d381e2ff4524578a30a38917c
akakou <akakou571@gmail.com>
2020-07-03 16:11:51 +0900

    [add] Auto generate form

---
+++ public/api/question.php
@@ -0,0 +1,16 @@
+<?php
+require  __DIR__ . '/../../vendor/autoload.php';
+
+require __DIR__ . '/../../src/auth.php';
+require __DIR__ . '/../../src/db.php';
+
+?>
+
+[
+    {
+        "question": "value1"
+    },
+    {
+        "question": "value2"
+    }
+]
