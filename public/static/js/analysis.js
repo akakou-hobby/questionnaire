45a5716698bfc8fbf15a6c406d09fb3e0179ceb0
akakou <akakou571@gmail.com>
2020-07-03 13:06:47 +0900

    [add] Analysis answers

--- public/static/js/analysis.js
+++ public/static/js/analysis.js
@@ -1,14 +1,26 @@
-const analysis = () => {
-  return [
-    {
-      question: "a",
-      answer: "b",
-      count: "c",
-    },
-    {
-      question: "a",
-      answer: "b",
-      count: "c",
-    },
-  ];
+const analysis = (array) => {
+  const results = [];
+
+  array.forEach((data) => {
+    const count = array.reduce(
+      (p, x) => p + (data.question == x.question && data.answer == x.answer),
+      0
+    );
+
+    if (count == 0) return;
+
+    array = array.filter(
+      (x) => data.question != x.question || data.answer != x.answer
+    );
+
+    const result = {
+      question: data.question,
+      answer: data.answer,
+      count: Number(count),
+    };
+
+    results.push(result);
+  });
+
+  return results;
 };
