1b3b6ccfbee535c6ddb05ab9f916044b60fb716b
akakou <akakou571@gmail.com>
2020-07-23 22:19:46 +0900

    [update] Remove axios configuration

--- public/static/js/axios.js
+++
@@ -1,6 +0,0 @@
-axios.interceptors.request.use(async (request) => {
-  if (request.url != "api/answers/create.php") {
-    request.headers["Authorization"] = `Bearer ${firebaseUserIdToken}`;
-  }
-  return request;
-});
